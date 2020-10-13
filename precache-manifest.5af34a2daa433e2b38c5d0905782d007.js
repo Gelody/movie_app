70acf3baf8d5cac00c89853e0be0acc58cfef0b7
Angelo <bbirds94@gmail.com>
2020-10-13 13:11:54 +0900

    Updates

---
+++ precache-manifest.5af34a2daa433e2b38c5d0905782d007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7889ce43a34066ff774604253c46512",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "69e52d8add3649558d4d",
+    "url": "/movie_app_2020/static/css/main.01ae5292.chunk.css"
+  },
+  {
+    "revision": "e8e713a700334ff78fe9",
+    "url": "/movie_app_2020/static/js/2.74d8efcf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.74d8efcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e52d8add3649558d4d",
+    "url": "/movie_app_2020/static/js/main.f8a74fa3.chunk.js"
+  },
+  {
+    "revision": "4839ee59ec104f2d85b3",
+    "url": "/movie_app_2020/static/js/runtime-main.2f3c173d.js"
+  }
+]);
